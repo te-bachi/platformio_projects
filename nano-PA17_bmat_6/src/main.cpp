8d312ce63fa98a6bc3c8e55c6fca880856dacb72
Andreas Bachmann <bachi@te-clan.ch>
2017-11-24 16:05:36 +0100

    nano-PA17_bmat_6

--- nano-PA17_bmat_6/src/main.cpp
+++ nano-PA17_bmat_6/src/main.cpp
@@ -6,24 +6,34 @@
  * https://github.com/te-bachi/nano-PA17_bmat_6
  */
 
+#include <Arduino.h>
+#include <Wire.h>
 #include "Leishman.h"
 
+void receiveEvent(int numBytes);
 void zcd_isr();
 
 uint32_t i;
 
 const byte interruptPin = 2;
 
+
 void
-setup() {
+setup()
+{
     i = 0;
     attachInterrupt(digitalPinToInterrupt(interruptPin), zcd_isr, RISING);
     Leishman::begin();
+
+    Wire.begin(8);
+    Wire.onReceive(receiveEvent);
+
     Serial.println("Done!");
 }
 
 void
-loop() {
+loop()
+{
     //int k;
 
     cli.check();
@@ -31,6 +41,19 @@ loop() {
 
     String s = "i = " + String(i);
     Serial.println(s);
+}
+
+void
+receiveEvent(int numBytes) {
+    Serial.print("I2C (bytes=");
+    Serial.print(numBytes);
+    Serial.print(") ");
+    while (1 < Wire.available()) {
+        char c = Wire.read();
+        Serial.print(c);
+    }
+    byte x = Wire.read();
+    Serial.println(x);
 }
 
 void
